f9927a58e6e343537f1d40194fec311804c37948
1 <1>
2018-01-11 11:12:20 +0100

    mattre la Dal a jour

--- GrandHotel/GrandHotel/DAL.cs
+++ GrandHotel/GrandHotel/DAL.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GrandHotel.Entites;
 
 namespace GrandHotel
 {
     public class DAL : DbContext
     {
+        public DAL() : base("GrandHotel.Properties.Settings.HotelConnection")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
         public IList<Client> ObtenirClients()
         {
+
 
             //var listClients = new List<Client>();
 
