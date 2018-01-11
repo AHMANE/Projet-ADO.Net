4b23eb16330d94e027e318e6d745ea8c923195d0
quikhanhmac <quikhanhmac@gmail.com>
2018-01-11 19:33:09 +0100

    xcvxcv

--- GrandHotel/GrandHotel/Entites.cs
+++ GrandHotel/GrandHotel/Entites.cs
@@ -9,7 +9,7 @@ using System.Xml.Serialization;
 
 namespace GrandHotel
 {
-   public class Entites //
+   public class Entites 
     {
         public class Client
         {
@@ -26,8 +26,8 @@ namespace GrandHotel
             public bool CarteFidelite { get; set; }
             [XmlAttribute]
             public string Societe { get; set; }
-            
-            // propriete de navigation
+
+            // Propriété de navigation
             [Display(ShortName = "None")]
             public virtual Adresse Adresses { get; set; }
             [Display(ShortName = "None")]
@@ -57,7 +57,8 @@ namespace GrandHotel
             public string CodeType { get; set; }
             [XmlIgnore]
             public bool Pro { get; set; }
-            // propriete de navigation
+            // Propriété de navigation
+            [Display(ShortName = "None")]
             public virtual Client Client { get; set; }
         }
          public class Adresse 
@@ -74,8 +75,12 @@ namespace GrandHotel
             public string CodePostal { get; set; }
             [XmlAttribute]
             public string Ville { get; set; }
-            // propriete de navigation
+            // Propriété de navigation
             public virtual Client Client { get; set; }
+            public override string ToString()
+            {
+                return IdClient + "   " + Rue + "   " + Complement + "   " + CodePostal + "   " + Ville;
+            }
 
         }
         public class Email
@@ -88,7 +93,7 @@ namespace GrandHotel
             public int IdClient { get; set; }
             [XmlIgnore]
             public bool Pro { get; set; }
-            // propriete de navigation
+            // // Propriété de navigation
             public virtual Client Client { get; set; }
         }
 
